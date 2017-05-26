2f58107bb0795aaab6aa990a2981f0854425d08d
மதி <tamilvendhan@TMP.local>
2017-05-26 14:09:05 +0530

    fixed config

--- src/config.js
+++ src/config.js
@@ -4,9 +4,8 @@ let config = {
 		"port": 3001
 	}
 };
-console.log(process.env);
 if(process.env.NODE_ENV === 'production') {
 	config.server.port = 80;
-	config.server.host = 'https://guarded-thicket-49148.herokuapp.com/';
+	config.server.host = 'guarded-thicket-49148.herokuapp.com';
 }
 export default config;
