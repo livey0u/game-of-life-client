b96c2626a64703696f570e999edf2a6df60f263f
மதி <tamilvendhan@TMP.local>
2017-05-26 16:17:10 +0530

    updated config for heroku

--- src/config.js
+++ src/config.js
@@ -1,11 +1,13 @@
 let config = {
 	"server": {
 		"host": "localhost",
-		"port": 3001
+		"port": 3001,
+		"url": `ws://localhost:3001`
 	}
 };
 if(process.env.NODE_ENV === 'production') {
 	config.server.port = 80;
 	config.server.host = 'guarded-thicket-49148.herokuapp.com';
+	config.server.url = `wss://${config.server.host}`;
 }
 export default config;
