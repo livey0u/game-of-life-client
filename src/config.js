eb8f6e8db71e52161533a152641f887ed41bf289
மதி <tamilvendhan@TMP.local>
2017-06-07 08:00:08 +0530

    updated production heroku url in config

--- src/config.js
+++ src/config.js
@@ -5,9 +5,14 @@ let config = {
 		"url": `ws://localhost:3001`
 	}
 };
+// if(process.env.NODE_ENV === 'production') {
+// 	config.server.port = 80;
+// 	config.server.host = '139.59.61.233';
+// 	config.server.url = `ws://${config.server.host}`;
+// }
 if(process.env.NODE_ENV === 'production') {
-	config.server.port = 80;
-	config.server.host = '139.59.61.233';
+	config.server.port = 443;
+	config.server.host = 'guarded-thicket-49148.herokuapp.com';
 	config.server.url = `ws://${config.server.host}`;
 }
 export default config;
