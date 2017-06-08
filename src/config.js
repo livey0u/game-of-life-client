766442f33fb59b39a3d90226aefa5f1bcd762a23
மதி <tamilvendhan@TMP.local>
2017-06-08 08:46:45 +0530

    local relay server url config updated

--- src/config.js
+++ src/config.js
@@ -2,14 +2,9 @@ let config = {
 	"server": {
 		"host": "localhost",
 		"port": 3001,
-		"url": `ws://localhost:5100`
+		"url": `ws://localhost:3001`
 	}
 };
-// if(process.env.NODE_ENV === 'production') {
-// 	config.server.port = 80;
-// 	config.server.host = '139.59.61.233';
-// 	config.server.url = `ws://${config.server.host}`;
-// }
 if(process.env.NODE_ENV === 'production') {
 	config.server.port = 443;
 	config.server.host = 'game-of-life-relay-server.herokuapp.com';
